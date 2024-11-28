c4b4b1b839d69088504603c81a8201753485f54c
Filip <82238474+jumperdevweb3@users.noreply.github.com>
2024-11-28 20:49:56 +0100

    Update page.tsx
    init commit

--- src/app/purchase/page.tsx
+++ src/app/purchase/page.tsx
@@ -1,16 +1,14 @@
-import { EmailLoginForm } from "@/components/email-login-form";
 import { Metadata } from "next";
-import { getTicketDetails } from "@/actions";
 
 export const metadata: Metadata = {
   title: "Blessed.fan | fund wallet"
 };
 
 export default async function PurchasePage() {
-  const ticketDetails = await getTicketDetails();
-  console.log("🎫 ticketDetails: ", ticketDetails)
+  // const ticketDetails = await getTicketDetails();
+  // console.log("🎫 ticketDetails: ", ticketDetails)
 
-  return (
-    <EmailLoginForm ticketDetails={ticketDetails} />
+  return (<div></div>
+    // <EmailLoginForm ticketDetails={ticketDetails} />
   );
 }
